26c3cb5e2ef06cb4bbde769215a01af677aaddf0
scott <smasherprog@gmail.com>
2022-04-02 10:00:39 -0700

    removed submodle

---
+++ include/Alex.h
@@ -0,0 +1,18 @@
+﻿// Alex.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_vulkan.h"
+#include <stdio.h>          // printf, fprintf
+#include <stdlib.h>         // abort
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+#include <assimp/Importer.hpp>
+#include <assimp/config.h>
+#include "sail/src/bindings/c++/sail-c++.h"
+
+#include <iostream>
